d967ecf7fdcdf4262c726739a309f680221e7f5f
VladFominov <111357638+VladFominov@users.noreply.github.com>
2022-10-17 13:05:22 +0300

    task-06-complete

---
+++ src/js/task-05.js
@@ -0,0 +1,13 @@
+const inputRef = document.querySelector("#name-input")
+console.log(inputRef);
+const spanRef = document.querySelector("#name-output")
+console.log(spanRef);
+
+inputRef.addEventListener('input', (e) => {
+    spanRef.textContent = e.target.value;
+    inputRef.classList.remove();
+})
+
+// Напиши скрипт, який під час набору тексту в інпуті input#name - input(подія input),
+// підставляє його поточне значення в span#name - output.Якщо інпут порожній,
+// у спані повинен відображатися рядок "Anonymous".
---
+++ src/js/task-06.js
@@ -0,0 +1,38 @@
+const inputRef = document.querySelector('#validation-input')
+console.log(inputRef)
+
+inputRef.addEventListener('input', (e) => {
+    
+})
+
+const dataLength = Number(inputRef.dataset.length);
+
+const isInputValid = (e) => {
+    inputRef.classList.remove('valid', 'invalid');
+    
+    if (dataLength === inputRef.value.length) {
+          
+        inputRef.classList.add('valid')
+    }
+  else  {
+        inputRef.classList.add('invalid')
+    }  
+}
+inputRef.addEventListener('blur',isInputValid)
+
+
+
+
+
+
+
+
+
+
+
+// Напиши скрипт, який під час втрати фокусу на інпуті(подія blur),
+// перевіряє його вміст щодо правильної кількості введених символів.
+// Яка кількість смиволів повинна бути в інпуті, зазначається в його атрибуті data-length.
+// Якщо введена правильна кількість символів, то border інпуту стає зеленим,
+// якщо неправильна кількість - червоним.
+// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
